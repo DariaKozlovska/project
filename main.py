1653378c2da7f2ab5f219b803648919fabced103
DariaKozlovska <odriy.lovska@gmail.com>
2024-05-27 22:01:57 +0200

    create-yaml-file-function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 import os
+import yaml
 
 def get_args():
     arg_parser = argparse.ArgumentParser()
@@ -40,6 +41,51 @@ def save_json(data, file_path):
 
         with open(file_path, 'w') as file:
             json.dump(data, file, indent=4)
-        print("Data saved successfully.")
     except Exception as e:
         print("Error: ", e)
+
+def yaml_file(file_path):
+    if not os.path.exists(file_path):
+        print("File not found: ", file_path)
+        return None
+
+    try:
+        with open(file_path, 'r') as file:
+            data = yaml.safe_load(file)
+            return data
+    except yaml.YAMLError as e:
+        print("Error decoding YAML: ", e)
+    except Exception as e:
+        print("Error: ", e)
+    
+    return None
+
+def main():
+    args = get_args()
+    input_file = args.input_file
+    output_file = args.output_file
+
+    if input_file.lower().endswith('.json'):
+        load_function = json_file
+    elif input_file.lower().endswith('.yaml') or input_file.lower().endswith('.yml'):
+        load_function = yaml_file
+    else:
+        print("Unsupported file format. Currently, only .json, .yaml/.yml, and .xml files are supported for reading.")
+        return
+
+    data = load_function(input_file)
+    if data is None:
+        print("Failed to load data.")
+        return
+
+
+    if output_file.lower().endswith('.json'):
+        save_json(data, output_file)
+    else:
+        print("Unsupported file format. Currently, only .yaml/.yml, .json, and .xml files are supported for writing.")
+        return
+
+    print("Data successfully saved to ", output_file)
+
+if __name__ == "__main__":
+    main()
